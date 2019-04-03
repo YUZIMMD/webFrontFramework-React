64954ff5b314ed4a02c23d3b565a93d93964502e
dengyt <2248545525@qq.com>
2019-04-03 16:30:54 +0800

    基础版

---
+++ client/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+          时间多久啊
+      </div>
+    )
+  }
+}
+
+export default App
